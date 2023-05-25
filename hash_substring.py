c3e7bf7655294115222be9c5b0fd923e3871ddc0
agaspars <82602542+agaspars@users.noreply.github.com>
2023-05-25 21:12:35 +0000

    hash function implemented

--- hash_substring.py
+++ hash_substring.py
@@ -1,4 +1,15 @@
 # python3
+import random
+
+_multiplier = random.randint(10, 1000)
+_prime = 100010
+
+
+def hash_func(s):
+    ans = 0
+    for c in reversed(s):
+        ans = (ans * _multiplier + ord(c)) % _prime
+    return ans
 
 def read_input():
     # this function needs to aquire input both from keyboard and file
@@ -14,7 +25,7 @@ def read_input():
     elif choice == 'F':
         filename = 'tests/06'
         with open(filename, 'r') as test:
-            pattern = test.readline().rstrip()
+            pattern = test.readline().rstrip() #readline, remove endline symbols
             text = test.readline().rstrip()
     # return both lines in one return
     # this is the sample return, notice the rstrip function
@@ -28,9 +39,9 @@ def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
     occurrences = []
     patternLen, textLen = len(pattern), len(text)
-    patternHash = hash(pattern)
+    patternHash = hash_func(pattern)
     for i in range(textLen - patternLen + 1):
-        if hash(text[i:i+patternLen]) == patternHash and text[i:i+patternLen] == pattern: #check whether the hash values of chosen text part and pattern are equal, and characters in the pattern and the substring are equal
+        if hash_func(text[i:i+patternLen]) == patternHash and text[i:i+patternLen] == pattern: #check whether the hash values of chosen text part and pattern are equal, and characters in the pattern and the substring are equal
             occurrences.append(i)   #occurence met
     # and return an iterable variable
     return occurrences
